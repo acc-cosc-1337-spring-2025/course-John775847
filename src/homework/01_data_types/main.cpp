6736bd1115802c3178c440da28134bcab1679f7f
rob <rob@linux.local>
2025-02-04 17:28:53 -0600

    Homework 1 with namespace statement for cout and cin

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,14 +1,17 @@
 ﻿#include <iostream>
 #include "data_types.cpp"
 
+using std::cin;
+using std::cout;
+
 int main(){
 
 	int num1;
 	int num2;
-	std::cin >> num1;
-	std::cin >> num2;
+	cin >> num1;
+	cin >> num2;
 	int result = multiply_numbers(num1, num2); //FROM data_types.cpp
-	std::cout << "Youre result is " << result << "\n";
+	cout << "Youre result is " << result << "\n";
 
 	return 0;
 }
